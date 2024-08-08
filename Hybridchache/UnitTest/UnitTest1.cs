aa8d275bb7f50316bff0d837f8caafac5cd079e2
Eddie <edrismoavenghafouri@gmail.com>
2024-08-08 11:45:07 +0330

    Trying another unit testing

--- Hybridchache/UnitTest/UnitTest1.cs
+++ Hybridchache/UnitTest/UnitTest1.cs
@@ -1,13 +1,10 @@
 using System.Net;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
 using Hybridchache;
 using Hybridchache.Wrapper;
 using Microsoft.Extensions.Caching.Hybrid;
 using Moq;
 using Moq.Protected;
-using Xunit;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 public class WeatherServiceTests
 {
@@ -32,10 +29,7 @@ public class WeatherServiceTests
         {
             Id = 1,
             Name = city,
-            Weather = new List<Weather>
-            {
-                new Weather { Id = 800, Main = "Clear", Description = "clear sky", Icon = "01d" }
-            },
+            Weather = [new Weather { Id = 800, Main = "Clear", Description = "clear sky", Icon = "01d" }],
             Base = "stations",
             Visibility = 10000,
             Timezone = 16200
@@ -51,8 +45,8 @@ public class WeatherServiceTests
 
         var result = await _weatherService.GetCurrentWeatherAsync(city);
 
-        Assert.NotNull(result);
-        Assert.Equal(city, result.Name);
+        Assert.IsNotNull(result);
+        Assert.AreEqual(city, result.Name);
     }
 
     [Fact]
@@ -65,7 +59,7 @@ public class WeatherServiceTests
 
         var httpMessageHandlerMock = new Mock<HttpMessageHandler>();
         httpMessageHandlerMock.Protected()
-            .Setup<Task<HttpResponseMessage>>("SendAsync", 
+            .Setup<Task<HttpResponseMessage>>("SendAsync",
                 ItExpr.IsNull<HttpRequestMessage>(),
                 ItExpr.IsNull<CancellationToken>())
             .ReturnsAsync(new HttpResponseMessage
@@ -76,14 +70,13 @@ public class WeatherServiceTests
         var httpClient = new HttpClient(httpMessageHandlerMock.Object);
 
         _httpClientFactoryMock.Setup(x => x.CreateClient(
-            It.IsNotNull<string>()))
+                It.IsNotNull<string>()))
             .Returns(httpClient);
 
         // Act
         var result = await _weatherService.GetCurrentWeatherAsync(city);
 
         // Assert
-        Assert.Null(result);
+        Assert.IsNull(result);
     }
 }
-
